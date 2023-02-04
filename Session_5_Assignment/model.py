493b13cce58784b3bb160e2cb7056c4ff3b4ec38
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-04 11:00:27 +0530

    Update model.py

--- Session_5_Assignment/model.py
+++ Session_5_Assignment/model.py
@@ -1,3 +1,11 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim 
+from torchvision import datasets, transforms
+
+
 dropout_value = 0.07
 class Net(nn.Module):
   def __init__(self):
