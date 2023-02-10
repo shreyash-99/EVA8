38ca6531e60d692d07d780adc62504088eccf5a2
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-10 19:19:58 +0530

    Update utils.py

--- Session_6_Assignment/utils.py
+++ Session_6_Assignment/utils.py
@@ -1,30 +1,30 @@
 # import necessary libraries
 import matplotlib.pyplot as plt
 import torch
-from data_loader import unnormalize
+# from data_loader import unnormalize
 
-def plot_misclassified_images(model, test_loader, device):
-    all_misclassified_images = []
+# def plot_misclassified_images(model, test_loader, device):
+#     all_misclassified_images = []
 
-    model.eval()
+#     model.eval()
 
-    with torch.no_grad():
-        for data , labels in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            _, pred = torch.max(output, 1)
-            for i in range(len(pred)):
-                if pred[i] != target[i]:
-                    all_misclassified_images.append({'image': data[i], 'predicted_class': pred[i], 'correct_class': target[i]})
+#     with torch.no_grad():
+#         for data , labels in test_loader:
+#             data, target = data.to(device), target.to(device)
+#             output = model(data)
+#             _, pred = torch.max(output, 1)
+#             for i in range(len(pred)):
+#                 if pred[i] != target[i]:
+#                     all_misclassified_images.append({'image': data[i], 'predicted_class': pred[i], 'correct_class': target[i]})
 
-    fig = plt.figure(figsize=(15, 5))
-    for i in range(10):
-        sub = fig.add_subplot(2, 5, i+1)
-        misclassified_image = all_misclassified_images[i]
-        plt.imshow(misclassified_image['image'].cpu().numpy().squeeze(), cmap='gray', interpolation='none')
-        sub.set_title("Correct class: {}\nPredicted class: {}".format(misclassified_image['correct_class'], misclassified_image['predicted_class']))
-    plt.tight_layout()
-    plt.show()
+#     fig = plt.figure(figsize=(15, 5))
+#     for i in range(10):
+#         sub = fig.add_subplot(2, 5, i+1)
+#         misclassified_image = all_misclassified_images[i]
+#         plt.imshow(misclassified_image['image'].cpu().numpy().squeeze(), cmap='gray', interpolation='none')
+#         sub.set_title("Correct class: {}\nPredicted class: {}".format(misclassified_image['correct_class'], misclassified_image['predicted_class']))
+#     plt.tight_layout()
+#     plt.show()
 
 
 def compute_accuracy_graph(accuracies):
