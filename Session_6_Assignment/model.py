b0c980e8002d5bc449e230342f3cf3820db93f2c
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-10 12:48:12 +0530

    Update model.py

---
+++ Session_6_Assignment/model.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+!pip install torchsummary
+from torchsummary import summary
+
+
+def get_model():
+    return Net()
+
+
+def get_model_summary(model, device, image_dimensions):
+    model = model.to(device)
+    summary(model, input_size = (3,32,32))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # self.conv1 = nn.Conv2d(3, 6, 5)
+        # self.pool = nn.MaxPool2d(2, 2)
+        # self.conv2 = nn.Conv2d(6, 16, 5)
+        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # self.fc2 = nn.Linear(120, 84)
+        # self.fc3 = nn.Linear(84, 10)
+
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels = 3,out_channels = 32, kernel_size =(3,3), padding = 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(dropout_value) #32 -> 32
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels = 32,out_channels = 64,kernel_size = (3,3), padding = 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(dropout_value) #32->32
+        )
+        self.conv2_5 = nn.Sequential(
+            nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = (3,3), padding = 1 ),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(dropout_value),
+            nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = (1,1))
+        )
+        self.transition1 = nn.Sequential(
+            nn.Conv2d(in_channels = 32,out_channels = 32,kernel_size = (3,3), dilation = 8)
+        )
+        
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels = 32,out_channels =  64, kernel_size = (3,3), padding = 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(dropout_value) #16 -> 16
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels = 64,out_channels = 64,kernel_size = (3,3), padding = 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(dropout_value) #16->16
+        )
+        self.transition2 = nn.Sequential(
+            nn.Conv2d(in_channels = 64,out_channels = 128,kernel_size = (3,3), dilation = 4)
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels = 128,out_channels =  128,kernel_size = (3,3), padding = 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(dropout_value) #8 -> 8
+        )
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(in_channels = 128,out_channels = 128,kernel_size = (3,3), padding = 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(dropout_value) #8->8
+        )
+        self.transition3 = nn.Sequential(
+            nn.Conv2d(in_channels = 128,out_channels = 128,kernel_size = (3,3), dilation = 2)
+        )
+
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size = 4)
+        )
+        self.fc = nn.Linear(64,10)
+
+
+    def forward(self, x):
+        x = self.transition1(self.conv2(self.conv1(x)))
+        x = self.transition2(self.conv4(self.conv3(x)))
+        x = self.transition3(self.conv6(self.conv5(x)))
+        x = self.gap(x)
+        x = x.view(-1,64)
+        x = self.fc(x)
+        return x
+
+
+net = Net()
