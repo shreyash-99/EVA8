67bbb3978df9d905d2adfcf70c8e3b5a4f1e8696
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-10 16:04:37 +0530

    Update training_code.py

--- Session_6_Assignment/training_code.py
+++ Session_6_Assignment/training_code.py
@@ -10,23 +10,20 @@ import matplotlib.pyplot as plt
 
 class train:
     def __init__(self):
-        
-        self.train_loss = []
-        self.train_acc = []
+
         self.train_accuracies = []
         self.train_losses = []
-        
 
     def execute_training(self, model, trainloader, device, optimiser, criterion, epoch):
         model.train()
         if epoch == 0:
             self.train_accuracies = [0] * 50
             self.train_losses = [0] * 50
-        train_loss = 0
         correct = 0
-        #total = 0
         processed = 0
         pbar = tqdm(trainloader)
+        training_loss_this_epoch = 0
+        training_acc_this_epoch = 0
 
         for batch_idx, (data, target) in enumerate(pbar):
             #get samples
@@ -40,22 +37,23 @@ class train:
 
             #calculate loss
             loss = criterion(y_pred, target)
-            self.train_losses.append(loss)
+            training_loss_this_epoch += loss
 
             # Backpropagation
             loss.backward()
             optimiser.step()
 
-            train_loss += loss.item()
+            # train_loss += loss.item()
 
             pred = y_pred.argmax(dim = 1, keepdim = True) # gets the index of the max log-probabilirty
             correct += pred.eq(target.view_as(pred)).sum().item()
             processed += len(data)
 
             pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-            self.train_acc.append(100*correct/processed)
+            training_acc_this_epoch += 100*correct/processed
 
-        self.train_accuracies[epoch] = self.train_acc[-1]
-        self.train_losses[epoch] = self.train_loss[-1]
+        self.train_accuracies[epoch] = 100 * pred.eq(target.view_as(pred)).sum().item()/ len(data)
+        self.train_losses[epoch] = training_loss_this_epoch
+
         return 
 
