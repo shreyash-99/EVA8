5053e133178161e000ff530d45c8bd040bcc7d0e
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-13 20:30:56 +0530

    Create data_loader3.py

---
+++ Session_6_Assignment/data_loader3.py
@@ -0,0 +1,49 @@
+
+import torch                   #PyTorch base libraries
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from torch.utils.data import Dataset, DataLoader
+
+import cv2
+import albumentations as A
+
+from albumentations import Compose, PadIfNeeded, RandomCrop, Normalize, HorizontalFlip, ShiftScaleRotate, CoarseDropout
+from albumentations.pytorch.transforms import ToTensorV2
+
+class Albumentation_cifar_Dataset(Dataset):
+  def __init__(self, image_list, train= True):
+      self.image_list = image_list
+      self.aug = A.Compose({
+          A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+          A.HorizontalFlip(),
+          A.ShiftScaleRotate(),
+          A.CoarseDropout(1, 16, 16, 1, 16, 16,fill_value=0.473363, mask_fill_value=None),
+          A.ToGray()
+      })
+
+      self.norm = A.Compose({A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+      })
+      self.train = train
+        
+  def __len__(self):
+      return (len(self.image_list))
+
+  def __getitem__(self, i):
+      
+      image, label = self.image_list[i]
+      
+      if self.train:
+        #apply augmentation only for training
+        image = self.aug(image=np.array(image))['image']
+      else:
+        image = self.norm(image=np.array(image))['image']
+      image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+      return torch.tensor(image, dtype=torch.float), label
+
+
+
+
+
+
