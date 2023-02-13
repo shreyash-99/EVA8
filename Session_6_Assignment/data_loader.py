a5d2b4040e36fa3a417f79d7f53499c00a621886
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-13 19:28:26 +0530

    minor changes

--- Session_6_Assignment/data_loader.py
+++ Session_6_Assignment/data_loader.py
@@ -47,27 +47,3 @@ class Albumentation_Dataset(Dataset):
 
 
 def train_loader():
-    train_transform = A.Compose(
-        [
-            A.HorizontalFlip(p=0.5),
-            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
-            A.Normalize((0.4914, 0.4822, 0.4471), (0.2469, 0.2433, 0.2615)),
-            A.Cutout(num_holes = 1, max_h_size = 16, max_w_size = 16, fill_value = 0.5, always_apply = False, p = 0.5)
-        ]
-    )
-
-def info_about_dataset(exp, train = True):
-    exp_data = exp.data
-
-    # Calculate the mean and std for normalization
-    if train:
-        print("[Train")
-    else :
-        print("Test")
-    print(' - Numpy Shape:', exp_data.shape)
-    print(' - min:', np.min(exp_data, axis=(0,1,2)) / 255.)
-    print(' - max:', np.max(exp_data, axis=(0,1,2)) / 255.)
-    print(' - mean:', np.mean(exp_data, axis=(0,1,2)) / 255.)
-    print(' - std:', np.std(exp_data, axis=(0,1,2)) / 255.)
-    print(' - var:', np.var(exp_data, axis=(0,1,2)) / 255.)
-
