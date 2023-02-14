5503df500a0fbbd27ae7997714afdf4d1053bb72
Shreyash Gupta <gshreyash2003@gmail.com>
2023-02-14 12:24:21 +0530

    Update main2.py

--- EVA8-pytorch/main2.py
+++ EVA8-pytorch/main2.py
@@ -134,3 +134,23 @@ def train_network(model, device, trainloader, testloader, EPOCHS, criterion = nn
     print('Finished Training')
 
     return trainObj, testObj
+
+def calClassAccuracy(net, dataloader, classes, device):  
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            images,labels = images.to(device),labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
